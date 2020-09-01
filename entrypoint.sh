daa1f1fd66a3b9893f09ef3196b56a4318d2f3ff
DenisPalnitsky <palnitsky@gmail.com>
2020-09-01 20:30:26 +0200

    Add pattern param

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,8 @@
 #!/bin/sh -l
 
-echo "Hello $1"
+echo "Pattern from action $1"
 
-/testhub-cli upload -o test-org -p cli-test -f **/TEST-com.mkyong.examples.TestMessageBuilder.xml -b $GITHUB_RUN_NUMBER -r $GITHUB_WORKSPACE
+/testhub-cli upload -o test-org -p cli-test -f $1 -b $GITHUB_RUN_NUMBER -r $GITHUB_WORKSPACE
 
 time=$(date)
 echo "::set-output name=time::$time"
