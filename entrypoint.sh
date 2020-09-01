d1779d5dcc713fb091ecfca21f07f3af50d2a759
DenisPalnitsky <palnitsky@gmail.com>
2020-09-01 23:58:44 +0200

    Set project name

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,9 +1,9 @@
 #!/bin/sh -l
 
 echo "Pattern from action $1"
-ls -R $GITHUB_WORKSPACE
+ 
 
-/testhub-cli upload -o test-org -p cli-test -f $1 -b $GITHUB_RUN_NUMBER -r $GITHUB_WORKSPACE
+/testhub-cli upload -p $GITHUB_REPOSITORY -f $1 -b $GITHUB_RUN_NUMBER -r $GITHUB_WORKSPACE
 
 time=$(date)
 echo "::set-output name=time::$time"
