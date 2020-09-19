73c41ceff6387f8381cf7c6f40c0ad03a685ea31
Denis Palnitsky <palnitsky@gmail.com>
2020-09-19 11:05:14 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,7 +3,7 @@
 echo "Pattern from action $1"
  
 
-/testhub-cli upload -p $GITHUB_REPOSITORY -f $1 -b $GITHUB_RUN_NUMBER -r $GITHUB_WORKSPACE
+/testhub-cli upload -p $GITHUB_REPOSITORY -f $1 -b $GITHUB_RUN_NUMBER -r $GITHUB_WORKSPACE --branch $GITHUB_REF
 
 echo "Pattern from action 2:  $2"
 /testhub-cli uploadCoverage -p $GITHUB_REPOSITORY -f $2 -b $GITHUB_RUN_NUMBER -r $GITHUB_WORKSPACE
