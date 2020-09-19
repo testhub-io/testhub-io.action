e3a7d804bf58fcd44fba3fda0512814c3436c7e8
Denis Palnitsky <palnitsky@gmail.com>
2020-09-19 16:18:39 +0200

    Execute in sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,11 +1,13 @@
-#!/bin/sh -l
+#!/bin/bash
 
+echo "Starting test results upload..."
 echo "Pattern for test results: $INPUT_TEST_RESULT_PATTERN" 
-testhub-cli upload -p $GITHUB_REPOSITORY -f $INPUT_TEST_RESULT_PATTERN -b $GITHUB_RUN_NUMBER -r $GITHUB_WORKSPACE --branch $GITHUB_REF
+usr/bin/testhub-cli upload -p "$GITHUB_REPOSITORY" -f "$INPUT_TEST_RESULT_PATTERN" -b "$GITHUB_RUN_NUMBER" -r "$GITHUB_WORKSPACE" --branch "$GITHUB_REF"
 
 echo "Pattern for coverage: $INPUT_TEST_COVERAGE_PATTERN"
-testhub-cli uploadCoverage -p $GITHUB_REPOSITORY -f $INPUT_TEST_COVERAGE_PATTERN -b $GITHUB_RUN_NUMBER -r $GITHUB_WORKSPACE
+usr/bin/testhub-cli uploadCoverage -p "$GITHUB_REPOSITORY" -f "$INPUT_TEST_COVERAGE_PATTERN" -b "$GITHUB_RUN_NUMBER" -r "$GITHUB_WORKSPACE"
 
 time=$(date)
 echo "::set-output name=time::$time"
 
+echo "Finish results upload..."
