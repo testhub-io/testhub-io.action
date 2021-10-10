ef756d3d299d479c6f0c659b32422a9464f0e533
Denis Palnitsky <palnitsky@gmail.com>
2021-10-10 13:01:04 +0200

    Fix entrypoint directory

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,10 +2,10 @@
 
 echo "Starting test results upload..."
 echo "Pattern for test results: $INPUT_TEST_RESULT_PATTERN" 
-/testhub-cli upload -p "$GITHUB_REPOSITORY" -f "$INPUT_TEST_RESULT_PATTERN" -b "$GITHUB_RUN_NUMBER" -r "$(INPUT_BASE_DIRECTORY)" --branch "$GITHUB_REF"
+/testhub-cli upload -p "$GITHUB_REPOSITORY" -f "$INPUT_TEST_RESULT_PATTERN" -b "$GITHUB_RUN_NUMBER" -r "$INPUT_BASE_DIRECTORY" --branch "$GITHUB_REF"
 
 echo "Pattern for coverage: $INPUT_TEST_COVERAGE_PATTERN"
-/testhub-cli uploadCoverage -p "$GITHUB_REPOSITORY" -f "$INPUT_TEST_COVERAGE_PATTERN" -b "$GITHUB_RUN_NUMBER" -r "$(INPUT_BASE_DIRECTORY)"
+/testhub-cli uploadCoverage -p "$GITHUB_REPOSITORY" -f "$INPUT_TEST_COVERAGE_PATTERN" -b "$GITHUB_RUN_NUMBER" -r "$INPUT_BASE_DIRECTORY"
 
 time=$(date)
 echo "::set-output name=time::$time"
