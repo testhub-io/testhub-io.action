1991b521f9239503bb84e3e275c4e089392cf418
Denis Palnitsky <palnitsky@gmail.com>
2022-08-20 16:00:29 +0200

    Branch name

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,8 +3,11 @@
 echo "Starting test results upload..."
 echo "Pattern for test results: $INPUT_TEST_RESULT_PATTERN" 
 
-BRANCH_NAME=echo ${GITHUB_REF##*/}
+
+BRANCH_NAME="${GITHUB_HEAD_REF:-${GITHUB_REF_NAME}}"
+BRANCH_NAME="${BRANCH_NAME:-@undefined}"
 echo "Branch name: $BRANCH_NAME"
+
 /testhub upload -p "$GITHUB_REPOSITORY" -f "$INPUT_TEST_RESULT_PATTERN" -b "$GITHUB_RUN_NUMBER" -r "$INPUT_BASE_DIRECTORY" --branch "$BRANCH_NAME"
 
 echo "Pattern for coverage: $INPUT_TEST_COVERAGE_PATTERN"
